1530204d99739885914b5bc7a9480477cba41fe4
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-03 01:19:32 +0300

    Small supertest suite

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,13 +1,50 @@
-const request = require("supertest");
-const app = require("../../src/app");
+import request from "supertest";
+import app from "./app";
 
 describe("Test the root path", () => {
-  test("It should response the GET method", done => {
-    request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
+    test("It should response the GET method", () => {
+      return request(app)
+        .get("/")
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+        });
+    });
+  });
+
+  describe("Test the user-api", () => {
+    test("Getting all users should response GET", () => {
+        return request(app)
+          .get("/api/user")
+          .then(response => {
+            expect(response.statusCode).toBe(200);
+          });
+      });
+
+      test("Getting one users should response GET", () => {
+        return request(app)
+          .get("/api/user/1")
+          .then(response => {
+            expect(response.statusCode).toBe(200);
+          });
       });
   });
-});
+
+
+  describe("Test the book-api", () => {
+
+      test("It should response the GET method", () => {
+        return request(app)
+          .get("/api/books")
+          .then(response => {
+            expect(response.statusCode).toBe(200);
+          });
+      });
+
+      test("It should response the GET method", () => {
+        return request(app)
+          .get("/api/books/1")
+          .then(response => {
+            expect(response.statusCode).toBe(200);
+          });
+      });
+  });
